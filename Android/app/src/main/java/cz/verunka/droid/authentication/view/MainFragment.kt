c9b87542c1571ed4e36ac45fdfa3332346b6b492
Veronika Dzurikova <xvera6@gmail.com>
2022-10-28 13:24:51 +0200

    ✨ Add screens and resources in xamarin and android app

--- Android/app/src/main/java/cz/verunka/droid/authentication/view/MainFragment.kt
+++ Android/app/src/main/java/cz/verunka/droid/authentication/view/MainFragment.kt
@@ -1,34 +1,53 @@
 package cz.verunka.droid.authentication.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import cz.verunka.droid.authentication.R
+import cz.verunka.droid.authentication.databinding.MainFragmentBinding
 import cz.verunka.droid.authentication.viewModel.MainViewModel
 
+
 class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private val navController = Navigation.findNavController(activity!!, R.id.main_navigation)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding: MainFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.main_fragment,
+            container,
+            false
+        )
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        binding.mainButtonLogIn.setOnClickListener {
+
+            Log.i("MainFragment", "Log In pressed")
+            navController.navigate(R.id.tabs_root_destination)
+        }
+
+        return binding.root
+    }
 
     companion object {
         fun newInstance() = MainFragment()
     }
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        // TODO: Use the ViewModel
-    }
-
 }
--- Xamarin/XamarinAuthentication/src/XamarinAuthentication.iOS/AppDelegate.cs
+++ Xamarin/XamarinAuthentication/src/XamarinAuthentication.iOS/AppDelegate.cs
@@ -1,10 +1,22 @@
 ﻿using Foundation;
 using MvvmCross.Forms.Platforms.Ios.Core;
+using UIKit;
+using XamarinAuthentication.UI;
 
 namespace XamarinAuthentication.iOS
 {
     [Register(nameof(AppDelegate))]
     public partial class AppDelegate : MvxFormsApplicationDelegate<Setup, Core.App, UI.App>
     {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            Xamarin.Forms.PancakeView.iOS.PancakeViewRenderer.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+
+        }
     }
 }
