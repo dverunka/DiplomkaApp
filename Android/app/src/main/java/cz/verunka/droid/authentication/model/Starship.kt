e4aeefa4482772032349ae7ae3f01d0bf55a5696
Veronika Dzurikova <xvera6@gmail.com>
2022-10-28 13:04:03 +0200

    🎉 Implement base android app

---
+++ Android/app/src/main/java/cz/verunka/droid/authentication/model/Starship.kt
@@ -0,0 +1,9 @@
+package cz.verunka.droid.authentication.model
+
+data class Starship(
+    val name: String,
+    val type: String,
+    val year: Int,
+    val length: String,
+    val photo: String
+) {}
