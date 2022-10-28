bf7eca5c9648e07edc43ce63b78113da1dc734d4
Veronika Dzurikova <xvera6@gmail.com>
2022-10-28 12:46:37 +0200

    🎉 Implement base xamarin app

---
+++ Xamarin/XamarinAuthentication/src/XamarinAuthentication.Core/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace XamarinAuthentication.Core.Models
+{
+	public class User
+	{
+		public string Name { get; set; }
+
+		public string Hometown { get; set; }
+
+		public string Email { get; set; }
+
+		public string Photo { get; set; }
+
+		public string GitHubUrl { get; set; }
+
+		public string SteamUrl { get; set; }
+
+		public User(string name, string hometown, string photo)
+		{
+			this.Name = name;
+			this.Hometown = hometown;
+			this.Photo = photo;
+		}
+	}
+}
---
+++ Xamarin/XamarinAuthentication/src/XamarinAuthentication.Core/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using XamarinAuthentication.Core.Models;
+using XamarinAuthentication.Core.Services.DataService;
+
+namespace XamarinAuthentication.Core.ViewModels.Dashboard
+{
+	public class DashboardViewModel: MvxNavigationViewModel
+	{
+		private readonly IExampleDataService _exampleDataService;
+
+		public MvxObservableCollection<string> Fighters { get; set; } = new MvxObservableCollection<string>();
+
+		public MvxObservableCollection<Starship> Starships { get; set; } = new MvxObservableCollection<Starship>();
+
+		public DashboardViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IExampleDataService exampleDataService) 
+			: base(logProvider, navigationService)
+		{
+			this._exampleDataService = exampleDataService;
+		}
+
+		public override Task Initialize()
+		{
+			this.Fighters.AddRange(this._exampleDataService.GetFighters());
+			this.Starships.AddRange(this._exampleDataService.GetStarships());
+
+			return base.Initialize();
+		}
+	}
+}
